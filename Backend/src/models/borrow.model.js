4aea8ff78f5b1f743ec9c18c48024c993cc458d3
coderSubhadeepdeb <sbhdpdeb@gmail.com>
2026-01-30 03:35:39 +0530

    some models added

---
+++ Backend/src/models/borrow.model.js
@@ -0,0 +1,50 @@
+import mongoose , {Schema} from "mongoose";
+
+const borrowSchema = new Schema({
+        lenderName : {
+            type: String,
+            required: true,
+            trim: true
+        },
+        amount:{
+            type: Number,
+            required: true,
+            trim: true
+        },
+        lenderEmail:{
+            type: String,
+            trim: true
+        },
+        lenderContactNumber:{
+            type: String,
+            required: true,
+            trim: true
+        },
+        lenderUpiId:{
+            type: String,
+            trim: true
+        },
+        description:{
+            type: String
+        },
+        expectedReturnDay:{
+            type: Number,
+            required: true,
+            trim: true
+        },
+        setReminder:{
+            type: Boolean,
+            default: false
+        },
+        reminderPeriod:{
+            type: Number,
+            default: 0
+        }
+    },{
+
+        timestamps: true
+
+    });
+
+
+export const Borrow = mongoose.model("Borrow", borrowSchema);
---
+++ Backend/src/models/lend.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const lendSchema = new mongoose.Schema({
+    borrowerName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount:{
+        type: Number,
+        required: true,
+        trim: true
+    },
+    borrowerEmail:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    borrowerContactNumber:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    description:{
+        type : String,
+    },
+    expectedReturnDays: {
+        type: Number,
+        required: true
+    },
+    setReminderForBorrower:{
+        type: Boolean,
+        default: false
+    },
+    setReminderForYou:{
+        type: Boolean,
+        default: false
+    },
+    reminderPeriod:{
+        type: Number,
+        default: 0
+    },
+},{
+    timestamps: true
+});
+
+export const Lend = mongoose.model("Lend", lendSchema);
